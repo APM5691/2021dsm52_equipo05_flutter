928ef758f480b6465207ad1fa62606b2e5a72ee7
DSM52 Angel Palacios Mirafuentes <al221910938@gmail.com>
2021-01-24 20:47:01 -0600

    14 Making a request to posts

--- lib/screen/posts-screen.dart
+++ lib/screen/posts-screen.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:dio/dio.dart' as Dio;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/models/posts.dart';
+
+import '../dio.dart';
 
 class PostsScreen extends StatefulWidget {
   @override
@@ -9,6 +15,14 @@ class PostsScreen extends StatefulWidget {
 }
 
 class PostsState extends State<PostsScreen> {
+  Future<List<Post>> getPosts() async {
+    Dio.Response response = await dio().get('user/posts');
+
+    List posts = json.decode(response.toString());
+
+    return posts.map((post) => Post.fromJson(post)).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +30,25 @@ class PostsState extends State<PostsScreen> {
         title: Text("Posts"),
       ),
       body: Center(
-        child: Text("Posts"),
+        child: FutureBuilder<List<Post>>(
+          future: getPosts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    var item = snapshot.data[index];
+                    return ListTile(
+                      title: Text(item.title),
+                    );
+                  });
+            } else if (snapshot.hasError) {
+              log(snapshot.error.toString());
+              return Text('Failes to load posts');
+            }
+            return CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
