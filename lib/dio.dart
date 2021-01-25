4ad720d872f8218cd4b19218a3a41c1c592d1fba
DSM52 Angel Palacios Mirafuentes <al221910938@gmail.com>
2021-01-24 21:10:50 -0600

    15 Using dio interceptors

--- lib/dio.dart
+++ lib/dio.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/providers/auth.dart';
 
 Dio dio() {
   var dio = Dio(BaseOptions(
@@ -8,5 +9,18 @@ Dio dio() {
         'accept': 'application/json',
         'content-type': 'application/json'
       }));
+
+  dio
+    ..interceptors.add(InterceptorsWrapper(
+        onRequest: (options) => requestInterceptor(options)));
+
   return dio;
+}
+
+dynamic requestInterceptor(RequestOptions options) async {
+  if (options.headers.containsKey('auth')) {
+    var token = await Auth().getToken();
+
+    options.headers.addAll({'Authorization': 'Bearer $token'});
+  }
 }
