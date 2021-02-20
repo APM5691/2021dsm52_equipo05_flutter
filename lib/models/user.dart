012b9a31bd60f7e714487692ecb600e9b968f312
Miguel Betzay Dominguez Diaz DSM-52 <al221910421@gmail.com>
2021-02-19 21:29:51 -0600

    04 - Using a webservice.mp4

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:flutter_authentication_with_laravel_sanctum/webservice.dart';
+
 class User {
   int id;
   String name;
@@ -11,5 +14,17 @@ class User {
       name: json['name'],
       email: json['email'],
     );
+  }
+
+  String get fulldata {
+    return '${this.id} ${this.name} ${this.email}';
+  }
+
+  static Resourse get me {
+    return Resourse(
+        url: 'auth/me',
+        parse: (reponse) {
+          return User.fromJson(json.decode(reponse.body['data']));
+        });
   }
 }
---
+++ lib/webservice.dart
@@ -0,0 +1,16 @@
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+class Resource {
+  String url;
+  Function(Response response) parse;
+
+  Resource({this.url, this.parse});
+}
+
+class Webservice {
+  static Future load(Resource resource) async {
+    var response = await http.get('http://10.0.2.2:8000/api/${resource.url}');
+    return resource.parse(response);
+  }
+}
