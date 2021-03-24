5e27df2bd1945a8be8858ac103e6ad0bab682a2d
DSM52 Angel Palacios Mirafuentes <al221910938@gmail.com>
2021-03-23 18:36:19 -0600

    cambio a la barra de navegacion

--- lib/widgets/nav-drawer.dart
+++ lib/widgets/nav-drawer.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_authentication_with_laravel_sanctum/providers/auth.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/screen/crear-usuario-screen.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/screen/formulario-usuario-screen.dart';
 import 'package:flutter_authentication_with_laravel_sanctum/screen/login-screen.dart';
 import 'package:flutter_authentication_with_laravel_sanctum/screen/posts-screen.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/screen/productos_screen.dart';
 import 'package:flutter_authentication_with_laravel_sanctum/screen/usuarios-screen.dart';
-import 'package:flutter_authentication_with_laravel_sanctum/screen/vista-videos-screen.dart';
 import 'package:provider/provider.dart';
 
 class NavDrawer extends StatelessWidget {
@@ -15,64 +17,21 @@ class NavDrawer extends StatelessWidget {
         if (auth.authenticated) {
           return ListView(
             children: [
-              Card(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    const ListTile(
-                      leading: Icon(Icons.album),
-                      title: Text('The Enchanted Nightingale'),
-                      subtitle:
-                          Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        TextButton(
-                          child: const Text('BUY TICKETS'),
-                          onPressed: () {/* ... */},
-                        ),
-                        const SizedBox(width: 8),
-                        TextButton(
-                          child: const Text('LISTEN'),
-                          onPressed: () {/* ... */},
-                        ),
-                        const SizedBox(width: 8),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
               ListTile(
+                tileColor: Colors.orange,
+                leading: Icon(Icons.account_circle_rounded),
                 title: Text(auth.user.name),
+                subtitle: Text(auth.user.primerApellido +
+                    ' ' +
+                    auth.user.segundoApellido +
+                    ' ' +
+                    auth.user.sexo),
               ),
               ListTile(
                 title: Text("Posts"),
                 onTap: () {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => PostsScreen()));
-                },
-              ),
-              ListTile(
-                title: Text("Login"),
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()));
-                },
-              ),
-              ListTile(
-                title: Text("Logout"),
-                onTap: () {
-                  Provider.of<Auth>(context, listen: false).logout();
-                },
-              ),
-              ListTile(
-                title: Text("Vista Videos"),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => VistaVideosScreen()));
                 },
               ),
               ListTile(
@@ -83,7 +42,119 @@ class NavDrawer extends StatelessWidget {
                       MaterialPageRoute(
                           builder: (context) => UsuariosScreen()));
                 },
-              )
+              ),
+              ListTile(
+                title: Text("-----------------------------------------------"),
+              ),
+              ListTile(
+                title: Text("Lista de usuarios"),
+                onTap: () {
+                  print("Hola");
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => UsuariosScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Crear un nuevo usuario"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CrearUsuarioScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Formulario"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => FormularioUsuario()));
+                },
+              ),
+              ListTile(
+                title: Text("Productos"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductosScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Productos"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductosScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Productos"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductosScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Productos"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductosScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Productos"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductosScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Productos"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductosScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Productos"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductosScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Productos"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductosScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Productos"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductosScreen()));
+                },
+              ),
             ],
           );
         } else {
@@ -94,21 +165,20 @@ class NavDrawer extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
                     const ListTile(
-                      leading: Icon(Icons.album),
-                      title: Text('The Enchanted Nightingale'),
-                      subtitle:
-                          Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                      leading: Icon(Icons.account_circle_rounded),
+                      title: Text('hola'),
+                      subtitle: Text('hola'),
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: <Widget>[
                         TextButton(
-                          child: const Text('BUY TICKETS'),
+                          child: const Text('Iniciar sesion*'),
                           onPressed: () {/* ... */},
                         ),
                         const SizedBox(width: 8),
                         TextButton(
-                          child: const Text('LISTEN'),
+                          child: const Text('Registarse*'),
                           onPressed: () {/* ... */},
                         ),
                         const SizedBox(width: 8),
@@ -132,6 +202,16 @@ class NavDrawer extends StatelessWidget {
                 },
               ),
               ListTile(
+                title: Text("Posts"),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => PostsScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("-----------------------------------------------"),
+              ),
+              ListTile(
                 title: Text("Lista de usuarios"),
                 onTap: () {
                   print("Hola");
@@ -142,10 +222,30 @@ class NavDrawer extends StatelessWidget {
                 },
               ),
               ListTile(
-                title: Text("Posts"),
+                title: Text("Crear un nuevo usuario"),
                 onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => PostsScreen()));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CrearUsuarioScreen()));
+                },
+              ),
+              ListTile(
+                title: Text("Formulario"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => FormularioUsuario()));
+                },
+              ),
+              ListTile(
+                title: Text("Productos"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ProductosScreen()));
                 },
               ),
             ],
