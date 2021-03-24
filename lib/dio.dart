53379d65304514ba45cc34a8efca9f88dc531be7
DSM52 Angel Palacios Mirafuentes <al221910938@gmail.com>
2021-03-23 18:36:00 -0600

    cambio a laravel

--- lib/dio.dart
+++ lib/dio.dart
@@ -1,24 +1,24 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/providers/auth.dart';
 
 Dio dio() {
   var dio = Dio(BaseOptions(
-      baseUrl: 'https://6038f6534e3a9b0017e94461.mockapi.io/',
+      baseUrl: 'http://10.0.2.2:8000/api/',
       responseType: ResponseType.plain,
       headers: {
         'accept': 'application/json',
         'content-type': 'application/json'
       }));
 
-  // dio
-  //   ..interceptors.add(InterceptorsWrapper(
-  //       onRequest: (options) => requestInterceptor(options)))//
+  dio
+    ..interceptors.add(InterceptorsWrapper(
+        onRequest: (options) => requestInterceptor(options)));
   return dio;
 }
 
-// dynamic requestInterceptor(RequestOptions options) async {
-//   if (options.headers.containsKey('auth')) {
-//     var token = await Auth().getToken();
-
-//     options.headers.addAll({'Authorization': 'Bearer $token'});
-//   }
-// }
+dynamic requestInterceptor(RequestOptions options) async {
+  if (options.headers.containsKey('auth')) {
+    var token = await Auth().getToken();
+    options.headers.addAll({'Authorization': 'Bearer $token'});
+  }
+}
